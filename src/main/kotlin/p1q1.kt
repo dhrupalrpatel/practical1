ec64f96033f4f86b875468900f7bb522d12b0d11
dhrupal patel <dhrupalpatel20@gnu.ac.in>
2022-08-23 20:43:26 +0530

    prc1

---
+++ src/main/kotlin/p1q1.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    val a: Int = 22
+    val b: Float = 1.5F
+    var d: Char = 'D'
+    var c: String = "DHRUPAL"
+    val e: Boolean = false
+    val f: Double = 21.04
+    val g: Long = 991712345
+    val h: Short = -2
+    val i: Byte = 127
+    println("Integer value: $a")
+    println("Float value: $b")
+    println("String value: $c")
+    println("Character value: $d")
+    println("Boolean value: $e")
+    println("Double value: $f")
+    println("Long value: $g")
+    println("Short value: $h")
+    println("Byte value: $i")
+}
